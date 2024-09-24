0983be5eef2d079ccc6b2d70745c1b10380c074c
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-09-24 23:05:40 +0600

    Upload a Ludo Dice  Code

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ludo
+
+function rollDice() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+   
+    let imageSource = 'img/' + randomNumber + '.png';
+    document.getElementById('ludo').src = imageSource;
+}
